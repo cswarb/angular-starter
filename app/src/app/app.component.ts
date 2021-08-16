91590926e3ca76a3201580befb86c24b4913662d
luke-chant <luke.chant@autotrader.co.uk>
2021-08-16 16:46:27 +0100

    add child

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'test',
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'app';
+
+  help(test: any) {
+    return test;
+  }
 }
